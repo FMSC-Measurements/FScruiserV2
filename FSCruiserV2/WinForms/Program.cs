0a0236c5b39f51dd93d10509ad395a4dd2525141
Ben Campbell <benjaminjcampbell@fs.fed.us>
2015-10-21 17:01:36 -0600

    reorganize file locations

---
+++ FSCruiserV2/WinForms/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Logger;
+using FSCruiserV2.Logic;
+using System.Threading;
+using System.Xml.Serialization;
+using System.IO;
+using System.Reflection;
+using FSCruiserV2.Forms;
+using System.Diagnostics;
+
+namespace FSCruiserV2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+#if PocketPC
+        [MTAThread]
+#else
+        [STAThread]
+#endif
+        static void Main()
+        {
+            
+            AppDomain.CurrentDomain.UnhandledException += FMSC.Utility.ErrorHandling.ErrorHandlers.UnhandledException;
+            //PreJit();
+            using (ViewController viewController = new ViewController())
+            using (ApplicationController controller = new ApplicationController(viewController))
+            {
+                controller.Run();
+            }
+            Debug.Close();
+        }
+
+        //http://stackoverflow.com/questions/548915/preloading-assemblies
+        //private static void PreJit()
+        //{
+        //    ThreadPool.QueueUserWorkItem((t) =>
+        //    {
+        //        Thread.Sleep(1000); // Or whatever reasonable amount of time
+        //        try
+        //        {
+        //            XmlSerializer c = new XmlSerializer(typeof(object));
+        //        }
+        //        catch (Exception) { }
+
+        //        try
+        //        {
+        //            FormDataEntry de = new FormDataEntry();
+        //        }
+        //        catch { }
+
+        //        try
+        //        {
+        //            LayoutTreeBased l = new LayoutTreeBased();
+        //        }
+        //        catch { }
+
+        //    });
+        //}
+    }
+}
