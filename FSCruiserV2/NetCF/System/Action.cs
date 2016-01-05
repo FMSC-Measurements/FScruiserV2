981d733fc906e2300be42cdb1f254120a530652a
Ben Campbell <benjaminjcampbell@fs.fed.us>
2016-01-04 19:42:59 -0700

    add IEnumerableExtensions

--- FSCruiserV2/NetCF/System/Action.cs
+++ FSCruiserV2/NetCF/System/Action.cs
@@ -6,4 +6,8 @@ using System.Text;
 namespace System
 {
     public delegate void Action();
+
+    public delegate void Action<T>(T obj);
+
+    public delegate TResult Func< T, TResult>(T arg);
 }
---
+++ FSCruiserV2/NetCF/System/IEnumerableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq
+{
+    public static class IEnumerableExtensions
+    {
+        public static List<T> ToList<T>(this IEnumerable<T> source)
+        {
+            if (source == null) { throw new ArgumentNullException("source"); }
+            return new List<T>(source);
+        }
+
+        public static T[] ToArray<T>(this IEnumerable<T> source)
+        {
+            if (source == null) { throw new ArgumentNullException("source"); }
+            var list = new List<T>(source);
+            return list.ToArray();
+        }
+
+        public static bool Any<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+            foreach (T element in source)
+            {
+                if (predicate(element)) return true;
+            }
+            return false;
+        }
+    }
+}
