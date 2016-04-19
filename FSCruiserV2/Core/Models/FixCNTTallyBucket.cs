6f67be2fe3e332ca34179b744992b2bbcca3ed42
Ben Campbell <benjaminjcampbell@fs.fed.us>
2016-04-19 16:30:02 -0600

    stash

--- FSCruiserV2/Core/Models/FixCNTTallyBucket.cs
+++ FSCruiserV2/Core/Models/FixCNTTallyBucket.cs
@@ -9,20 +9,10 @@ namespace FSCruiser.Core.Models
     {
         public FixCNTTallyPopulation TallyPopulation { get; set; }
 
-        public PlotVM Plot { get; set; }
-
         public double InvervalValue { get; set; }
 
-        public int TreeCount 
-        {
-            get { return Trees.Count; }
-        }
+        public int TreeCount { get; set; }
 
-        public IList<TreeVM> Trees { get; set; }
 
-        public void Tally()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
