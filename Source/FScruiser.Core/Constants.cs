8f91aac775a4a6013dcc28ae9b6780067da04adf
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-01-31 17:35:35 -0700

    update version ids

--- Source/FScruiser.Core/Constants.cs
+++ Source/FScruiser.Core/Constants.cs
@@ -7,7 +7,7 @@ namespace FSCruiser.Core
     {
         public static bool NEW_SPECIES_OPTION = false;
 
-        public const string FSCRUISER_VERSION = "2016.12.07";
+        public const string FSCRUISER_VERSION = "2017.01.31";
 
         public const string APP_TITLE = "FScruiser - " + FSCRUISER_VERSION;
 
