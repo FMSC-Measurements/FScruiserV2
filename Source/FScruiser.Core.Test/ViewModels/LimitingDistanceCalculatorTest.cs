41fcb3f95af36423236cc9cbdc614876d4cb4886
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-02-24 18:21:15 -0700

    add/update tests

--- Source/FScruiser.Core.Test/ViewModels/LimitingDistanceCalculatorTest.cs
+++ Source/FScruiser.Core.Test/ViewModels/LimitingDistanceCalculatorTest.cs
@@ -17,15 +17,34 @@ namespace FScruiser.Core.Test.ViewModels
         [Fact]
         public void TestDeterminTreeInOrOut()
         {
-            LimitingDistanceCalculator.DeterminTreeInOrOut(.01, .01).Should().BeTrue();
+            //for tree to be in slope distance (first value) must be less than or equal to limiting distance (second value)
+            //both values are round to three decimal places
+            //here we test edge cases where both values should be determined to be equal
+            LimitingDistanceCalculator.DeterminTreeInOrOut(.001, .001).Should().BeTrue();
+            LimitingDistanceCalculator.DeterminTreeInOrOut(.0012, .0011).Should().BeTrue();
+            LimitingDistanceCalculator.DeterminTreeInOrOut(.0011, .0012).Should().BeTrue();
+            LimitingDistanceCalculator.DeterminTreeInOrOut(.002, .0015).Should().BeTrue();
+            LimitingDistanceCalculator.DeterminTreeInOrOut(.002, .0014).Should().BeFalse();
+        }
 
-            LimitingDistanceCalculator.DeterminTreeInOrOut(.012, .011).Should().BeTrue();
+        [Theory]
+        //variable radius
+        [InlineData(20.0, 10.0, 0, true, true, 19.028)]//to face
+        [InlineData(20.0, 10.0, 50, true, true, 21.274)]//to face, w/ 50% slope
+        [InlineData(20.0, 10.0, 0, true, false, 19.445)]//to center
+        //fixed
+        [InlineData(50.0, 10.0, 0, false, true, 16.236)]//to face
+        [InlineData(50.0, 10.0, 50, false, true, 18.152)]//to face, w/ 50% slope
+        [InlineData(50.0, 10.0, 0, false, false, 16.653)]//to center
+        public void TestCalculateLimitingDistance(double BAForFPS, double dbh, int slopePCT, bool isVar, bool isFace, double expected)
+        {
+            int sigDec = 3;
 
-            LimitingDistanceCalculator.DeterminTreeInOrOut(.011, .012).Should().BeTrue();
-
-            LimitingDistanceCalculator.DeterminTreeInOrOut(.02, .015).Should().BeTrue();
-
-            LimitingDistanceCalculator.DeterminTreeInOrOut(.02, .014).Should().BeFalse();
+            string measureTo = (isFace) ? LimitingDistanceCalculator.MEASURE_TO_FACE : LimitingDistanceCalculator.MEASURE_TO_CENTER;
+            var ld = LimitingDistanceCalculator.CalculateLimitingDistance(BAForFPS, dbh, slopePCT, isVar, measureTo);
+            ld = Math.Round(ld, sigDec);
+            expected = Math.Round(expected, 3);
+            ld.ShouldBeEquivalentTo(expected);
         }
     }
 }
