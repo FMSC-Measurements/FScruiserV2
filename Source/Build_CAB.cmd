2bcf3c68c6dad50278b8942557bae0279e42d398
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-03-03 19:03:18 -0700

    add build scripts for CAB and FDR installer

--- Source/Build_CAB.cmd
+++ Source/Build_CAB.cmd
@@ -1,10 +1,15 @@
 @ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+::name of this script
+SET me=%~n0
+::directory of script
+SET parent=%~dp0
 
- 
 SET devenvPath=C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe
-SET verStamp=%date:~10,4%%date:~4,2%%date:~7,2%
-SET buildLogPath=%cd%\pack.log
-SET cabOutDir=.\FSCruiserV2CECF20_CAB\Release\
+SET buildLogPath=%parent%\pack.log
+SET cabOutDir=%parent%\FSCruiserV2CECF20_CAB\Release\
+
+IF NOT DEFINED verStamp (SET verStamp=%date:~10,4%%date:~4,2%%date:~7,2%)
 
 ::if build log exists delete because devenv only appends to our log file
 IF EXIST "%buildLogPath%" DEL /Q "%buildLogPath%"
@@ -12,12 +17,13 @@ IF EXIST "%buildLogPath%" DEL /Q "%buildLogPath%"
 ::delete any existing cab file, to ensure we arnt working with a stale cab file
 IF EXIST "%cabOutDir%FScruiserV2.CAB" DEL /Q "%cabOutDir%FScruiserV2.CAB"
 
-"%devenvPath%" .\FScruiserV2.sln /build "Release|AnyCPU" /project "FSCruiserV2CECF20_CAB" /Out "%buildLogPath%"
+ECHO %me%:Start Building Cab
+"%devenvPath%" %parent%\FScruiserV2.sln /build "Release|AnyCPU" /project "FSCruiserV2CECF20_CAB" /Out "%buildLogPath%"
 
 ::display build log
 TYPE %buildLogPath%
 
-ECHO COPY /Y /B "%cabOutDir%FScruiserV2.CAB" "%cabOutDir%FScruiserV2_%verStamp%.CAB"
+COPY /Y /B "%cabOutDir%FScruiserV2.CAB" "%cabOutDir%FScruiserV2_%verStamp%.CAB"
 
-::
-EXIT %ERRORLEVEL%
+EXIT /B
+
---
+++ build_FDR.cmd
@@ -0,0 +1,22 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+
+:: script global variables
+IF NOT DEFINED verStamp (SET verStamp=%date:~10,4%%date:~4,2%%date:~7,2%)
+
+::detect if invoked via Window Explorer
+SET interactive=1
+ECHO %CMDCMDLINE% | FIND /I "/c" >NUL 2>&1
+IF %ERRORLEVEL% == 0 SET interactive=0
+
+::Build CAB File. This generates FScruiser.CAB and FScruiser_########.cab 
+CALL ./Source/Build_CAB
+
+::Build Inno Setup Installer
+SET appVer=%date:~10,4%.%date:~4,2%.%date:~7,2%
+
+"C:\Program Files (x86)\Inno Setup 5\iscc" /dAPP_VERSION=%appVer% /F"FScruiserV2_FDR_%verStamp%" "./Deployment/CFSetupScript.iss"  
+
+IF "%interactive%"=="0" PAUSE
+ENDLOCAL
+EXIT /B 0
