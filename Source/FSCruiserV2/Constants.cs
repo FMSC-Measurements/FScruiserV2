62e4c5519c8f65e415617db724074cecded51962
Ben Campbell <benjaminjcampbell@fs.fed.us>
2018-01-16 16:39:10 -0500

    update version ids

--- Source/FSCruiserV2/Constants.cs
+++ Source/FSCruiserV2/Constants.cs
@@ -7,7 +7,7 @@ namespace FSCruiser
 {
     public static class Constants
     {
-        public const string FSCRUISER_VERSION = "2017.06.22";
+        public const string FSCRUISER_VERSION = "2018.01.12";
 
         public const string APP_TITLE = "FScruiser - " + FSCRUISER_VERSION;
 
