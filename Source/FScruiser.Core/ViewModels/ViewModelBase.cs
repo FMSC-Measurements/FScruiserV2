0dc9138d72a6fedd5f7192942da407c10674f357
Ben Campbell <benjaminjcampbell@fs.fed.us>
2018-02-09 14:08:19 -0500

    remove INotifyPropertyChanging

--- Source/FScruiser.Core/ViewModels/ViewModelBase.cs
+++ Source/FScruiser.Core/ViewModels/ViewModelBase.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
 namespace FScruiser.Core.ViewModels
 {
     public class ViewModelBase : INotifyPropertyChanged
-#if !NetCF
-        , INotifyPropertyChanging
-#endif
     {
-
         protected void SetValue<T>(ref T target, T value, string propName)
         {
-#if !NetCF
-            NotifyPropertyChanging(propName);
-#endif
             target = value;
             NotifyPropertyChanged(propName);
         }
@@ -25,8 +14,6 @@ namespace FScruiser.Core.ViewModels
         {
             OnPropertyChanged(new PropertyChangedEventArgs(name));
         }
-
-        
 
         protected void OnPropertyChanged(PropertyChangedEventArgs e)
         {
@@ -37,34 +24,10 @@ namespace FScruiser.Core.ViewModels
             }
         }
 
-        
-
         #region INotifyPropertyChanged Members
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        #endregion
-
-#if !NetCF
-        #region INotifyPropertyChanging Members
-
-        protected void NotifyPropertyChanging(string name)
-        {
-            OnPropertyChanging(new PropertyChangingEventArgs(name));
-        }
-
-        protected void OnPropertyChanging(PropertyChangingEventArgs e)
-        {
-            var handle = PropertyChanging;
-            if (handle != null)
-            {
-                handle(this, e);
-            }
-        }
-
-        public event PropertyChangingEventHandler PropertyChanging;
-
-        #endregion
-#endif
+        #endregion INotifyPropertyChanged Members
     }
 }
